9e2c9596cd6ae1641a795c3945761604f260497a
Benjamin Van Ryseghem <benvr@wanadoo.fr>
2012-12-11 17:01:50 +0100

    started website

---
+++ WebPizza/website/WebPizza/login_files/login.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20121211
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('login_files/loginMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
